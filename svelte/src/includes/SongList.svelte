419abfa5864eb477e3b52694df735a81e47ce799
Francisco Gimeno <franchills@tuta.io>
2021-04-21 10:36:05 +0200

    Changed Song List Scroll to an extremely efficient solution

--- svelte/src/includes/SongList.svelte
+++ svelte/src/includes/SongList.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
+	import { onMount } from 'svelte'
+
 	import SongListItem from '../components/SongListItem.svelte'
 	import { scrollSongListToTop } from '../functions/scrollSongListToTop.fn'
 	import { selectedAlbumId, songListStore, selectedSongsStore } from '../store/final.store'
 
 	let isSelectedAlbumIdFirstAssign = true
+	let songsTrimmed = []
+	let scrollTime = 0
+	let progressValue = 0
+
+	const SONG_AMOUNT = 8
 
 	$: {
 		$selectedAlbumId
@@ -11,7 +18,24 @@
 			isSelectedAlbumIdFirstAssign = false
 		} else {
 			scrollSongListToTop()
+			scrollTime = 0
 		}
+	}
+
+	$: {
+		$songListStore
+		scrollTime
+		trimSongsArray()
+	}
+
+	function trimSongsArray() {
+		if ($songListStore.length - (scrollTime + 1) < SONG_AMOUNT) {
+			songsTrimmed = $songListStore.slice($songListStore.length - SONG_AMOUNT)
+		} else if (scrollTime >= 0) {
+			songsTrimmed = $songListStore.slice(scrollTime, -1).slice(0, SONG_AMOUNT)
+		}
+
+		setProgress()
 	}
 
 	let lastSelectedSong = 0
@@ -51,23 +75,73 @@
 			}
 		})
 	}
+
+	function scrollContainer(e: WheelEvent) {
+		scrollTime = scrollTime + Math.sign(e.deltaY)
+
+		if (scrollTime >= $songListStore.length) {
+			scrollTime = $songListStore.length
+		} else if (scrollTime < 0) {
+			scrollTime = 0
+		}
+	}
+
+	function setProgress() {
+		progressValue = ((100 / $songListStore.length) * scrollTime) | 0
+		document.documentElement.style.setProperty('--progress-bar-fill', `${progressValue}%`)
+	}
+
+	function handleScrollBarEvent(e) {
+		console.log(e)
+	}
+
+	onMount(() => {
+		let songListProgressBar = document.querySelector('song-list-progress-bar')
+
+		//TODO Add Event listeners MouseOver + MouseDown
+	})
 </script>
 
-<song-list-svlt on:click={(e) => selectSongs(e)}>
-	{#if $songListStore !== undefined}
-		{#each $songListStore as song, index (song.ID)}
-			<SongListItem {song} {index} />
-		{/each}
-	{/if}
+<song-list-svlt on:mousewheel={(e) => scrollContainer(e)} on:click={(e) => selectSongs(e)}>
+	<song-list>
+		{#if $songListStore !== undefined}
+			{#each songsTrimmed as song, index (song.ID)}
+				<SongListItem {song} {index} />
+			{/each}
+		{/if}
+	</song-list>
+	<song-list-progress-bar>
+		<progress-fill />
+	</song-list-progress-bar>
 </song-list-svlt>
 
 <style>
 	song-list-svlt {
-		border: 10px transparent solid;
 		grid-area: song-list-svlt;
-		overflow-y: auto;
-		height: 100%;
-		padding-right: 10px;
-		/* background-color: rgba(255,255,255,.20); */
+		display: grid;
+		grid-template-columns: auto max-content;
+	}
+
+	song-list {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-evenly;
+		padding: 0.5rem;
+	}
+
+	song-list-progress-bar {
+		cursor: pointer;
+		display: block;
+		width: 10px;
+		background-color: rgba(255, 255, 255, 0.15);
+	}
+
+	song-list-progress-bar progress-fill {
+		display: block;
+		width: auto;
+		background-color: rgba(255, 255, 255, 0.5);
+		height: var(--progress-bar-fill);
+		border-bottom: 5px solid rgba(255, 255, 255, 0.75);
+		max-height: 100%;
 	}
 </style>
