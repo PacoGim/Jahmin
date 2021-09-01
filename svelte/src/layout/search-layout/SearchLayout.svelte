9ecd7c347e8a58412d2841688add4bce16e304b5
Francisco Gimeno <franchills@tuta.io>
2021-09-01 13:16:30 +0200

    Search Designing

--- svelte/src/layout/search-layout/SearchLayout.svelte
+++ svelte/src/layout/search-layout/SearchLayout.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { onMount } from 'svelte'
+
 	import CoverArt from '../../components/CoverArt.svelte'
 	import SongListItem from '../../components/SongListItem.svelte'
 	import generateId from '../../functions/generateId.fn'
@@ -9,27 +11,21 @@
 	import { userSearchIPC } from '../../service/ipc.service'
 
 	import {
+		elementMap,
 		layoutToShow,
-		playingSongStore,
 		selectedAlbumId,
 		selectedSongsStore,
 		triggerGroupingChangeEvent,
 		triggerScrollToSongEvent
 	} from '../../store/final.store'
 
-	import type { SongFuzzySearchType, SongType } from '../../types/song.type'
+	import type { SongFuzzySearchType } from '../../types/song.type'
 
-	let debounce: NodeJS.Timeout = undefined
-
-	let searchString = 'rute'
+	let searchString = Math.random().toString(36).substring(8)
 	let foundSongs: SongFuzzySearchType[] = []
 
 	const filterOptions = ['Title', 'Album', 'Album Artist', 'Artist', 'Composer']
 	let selectedFilters = ['Title']
-
-	$: {
-		userTypingHandler(searchString, selectedFilters)
-	}
 
 	$: {
 		if (selectedFilters.length === 0) {
@@ -37,13 +33,10 @@
 		}
 	}
 
-	function userTypingHandler(searchString: string, selectedFilters: string[]) {
-		clearTimeout(debounce)
-		debounce = setTimeout(() => {
-			userSearchIPC(searchString, selectedFilters).then(result => {
-				foundSongs = result
-			})
-		}, 1000)
+	function sendSearchString() {
+		userSearchIPC(searchString, selectedFilters).then(result => {
+			foundSongs = result
+		})
 	}
 
 	function selectSong(selectedSong: SongFuzzySearchType) {
@@ -67,10 +60,23 @@
 	function getRootDir(value) {
 		return value.split('/').slice(0, -1).join('/')
 	}
+
+	onMount(() => {
+		sendSearchString()
+		window.addEventListener('keypress', ({ code }) => {
+			if (elementMap && $elementMap.get('SEARCH-LAYOUT') !== undefined && code === 'Enter') {
+				sendSearchString()
+			}
+		})
+	})
 </script>
 
-<search-layout class="layout" style="display:{$layoutToShow === 'Search' ? 'block' : 'none'}">
-	<input type="text" bind:value={searchString} />
+<search-layout class="layout" style="display:{$layoutToShow === 'Search' ? 'grid' : 'none'}">
+	<p>Press Enter or Search Button to search</p>
+	<search-input>
+		<input type="text" bind:value={searchString} />
+		<img src="./img/search.svg" alt="" on:click={() => sendSearchString()} />
+	</search-input>
 	<filter-selector>
 		{#each filterOptions as filter, index (index)}
 			<filter-check selected={selectedFilters.includes(filter)}>
@@ -81,7 +87,7 @@
 	</filter-selector>
 	<search-grid>
 		<grid-row>
-			<!-- Album Cover -->
+			<!-- ↓ Album Cover ↓ -->
 			<grid-value />
 			<grid-value>Title</grid-value>
 			<grid-value>Album</grid-value>
@@ -116,10 +122,87 @@
 
 <style>
 	search-layout {
-		/* grid-template-columns: auto; */
-		/* grid-template-rows: repeat(3,auto); */
-		/* display: grid; */
+		grid-template-columns: auto;
+		grid-template-rows: repeat(4, max-content);
+		display: grid;
 		overflow: auto;
+	}
+
+	search-input {
+		display: flex;
+		justify-content: center;
+		height: 3rem;
+	}
+
+	search-input input {
+		font-family: inherit;
+		font-size: inherit;
+		padding: 0 1rem;
+		text-align: center;
+		color: var(--primary-color);
+		background-color: rgba(128, 128, 128, 0.1);
+		border-radius: 3px 0 0 3px;
+		border: none;
+	}
+
+	search-input img {
+		cursor: pointer;
+		height: 100%;
+		background-color: rgba(128, 128, 128, 0.1);
+		border-radius: 0 3px 3px 0;
+		filter: invert(98%) sepia(99%) saturate(124%) hue-rotate(324deg) brightness(95%) contrast(93%);
+	}
+
+	filter-selector {
+		display: flex;
+		justify-content: center;
+	}
+
+	filter-selector filter-check {
+		margin: 0 0.5rem;
+		border-radius: 3px;
+		height: 1.75rem;
+		display: flex;
+	}
+
+	filter-selector filter-check input {
+		display: none;
+	}
+	filter-selector filter-check label {
+		display: inline-block;
+		padding: 0.25rem 0.5rem;
+		cursor: pointer;
+		color: #fff;
+		height: 1.75rem;
+		/* line-height: 1.75rem; */
+	}
+
+	filter-selector filter-check::after {
+		display: inline-block;
+		height: 1.75rem;
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
+			sans-serif;
+		border-radius: 0 3px 3px 0;
+		filter: brightness(1.25);
+		padding: 0 0.5rem;
+		line-height: 1.75rem;
+	}
+
+	filter-selector filter-check[selected='true'] {
+		background-color: forestgreen;
+	}
+
+	filter-selector filter-check[selected='true']::after {
+		background-color: forestgreen;
+		content: '✓';
+	}
+	filter-selector filter-check[selected='false'] {
+		background-color: crimson;
+	}
+
+	filter-selector filter-check[selected='false']::after {
+		background-color: crimson;
+		content: '✕';
 	}
 
 	search-grid {
@@ -138,6 +221,7 @@
 	grid-row:first-child {
 		cursor: default;
 		height: max-content;
+		font-variation-settings: 'wght' calc(var(--default-weight) + 200);
 	}
 
 	search-grid grid-row grid-value {
@@ -145,12 +229,5 @@
 		-webkit-line-clamp: 1;
 		-webkit-box-orient: vertical;
 		overflow: hidden;
-	}
-
-	filter-check[selected='true'] {
-		color: green;
-	}
-	filter-check[selected='false'] {
-		color: red;
 	}
 </style>
