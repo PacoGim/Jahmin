938354d7d643d8fd94c3e48937f7f4c6cdfe4466
Paco Gimeno <PacoGim@outlook.com>
2023-07-13 11:06:19 +0200

    Git cleanup

--- dist/main/context_menu/functions/addSeparator.fn.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function default_1(template) {
-    template.push({
-        type: 'separator'
-    });
-}
-exports.default = default_1;
--- dist/types/config.type.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ThemeOptions = void 0;
-var ThemeOptions;
-(function (ThemeOptions) {
-    ThemeOptions["SystemBased"] = "SystemBased";
-    ThemeOptions["Night"] = "Night";
-    ThemeOptions["Day"] = "Day";
-})(ThemeOptions = exports.ThemeOptions || (exports.ThemeOptions = {}));
